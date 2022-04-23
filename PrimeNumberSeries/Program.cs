21002cd39f76ab6ac962f6883dc8868048069035
Igor <ihor.kaseko@aploris.com>
2022-04-23 08:24:55 +0300

    Add project files.

---
+++ PrimeNumberSeries/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PrimeNumberSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            var n = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("{0} first prime numbers", n);
+            var count = 0u;
+            var i = 0u;
+            while (count < n)
+            {
+                if (IsPrimeNumber(i))
+                {
+                    Console.Write(i);
+                    if (count < n - 1)
+                    {
+                        Console.Write(", ");
+                    }
+                    count++;
+                }
+                i++;
+            }
+
+            Console.ReadLine();
+        }
+
+        public static bool IsPrimeNumber(uint n)
+        {
+            var result = true;
+
+            if (n > 1)
+            {
+                for (var i = 2u; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
