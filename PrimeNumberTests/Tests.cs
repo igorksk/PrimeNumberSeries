1d91fbbd99a2baa768c45d2aa90060ea0d5ce9ed
Igor Kaseko <igorksk@gmail.com>
2024-08-11 10:36:46 +0300

    Add tests

---
+++ PrimeNumberTests/Tests.cs
@@ -0,0 +1,62 @@
+using PrimeNumberSeries;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace PrimeNumberTests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void CreatePrimeNumbersSequence_ReturnsCorrectPrimesForN()
+        {
+            //TEST 1
+
+            // Arrange
+            uint n = 5;
+
+            // Act
+            var primes = PrimeNumbersHelpers.CreatePrimeNumbersSequence(n);
+
+            // Assert
+            CollectionAssert.AreEqual(new List<uint> { 2, 3, 5, 7, 11 }, primes);
+
+            //TEST 2
+
+            // Arrange
+            n = 0;
+
+            // Act
+            primes = PrimeNumbersHelpers.CreatePrimeNumbersSequence(n);
+
+            // Assert
+            CollectionAssert.AreEqual(new List<uint> {}, primes);
+        }
+
+        [TestMethod]
+        public void CreatePrimeNumbersSequence_IsPrimeNumberForN()
+        {
+            //TEST 1
+
+            // Arrange
+            uint n = 5;
+
+            // Act
+            var result = PrimeNumbersHelpers.IsPrimeNumber(n);
+
+            // Assert
+            Assert.IsTrue(result);
+
+            //TEST 2
+
+            // Arrange
+            n = 6;
+
+            // Act
+            result = PrimeNumbersHelpers.IsPrimeNumber(n);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
