0ddc8adf67a473cab6575fa8f841769b0ae95b52
Igor Kaseko <igorksk@gmail.com>
2024-08-11 09:50:52 +0300

    Separate method for numbers list

--- PrimeNumberSeries/Program.cs
+++ PrimeNumberSeries/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PrimeNumberSeries
 {
@@ -9,23 +11,41 @@ namespace PrimeNumberSeries
             Console.Write("N = ");
             var n = Convert.ToUInt32(Console.ReadLine());
             Console.WriteLine("{0} first prime numbers", n);
+            
+            var primeNumbers = CreatePrimeNumbersSequence(n);
+
+            if (primeNumbers.Count != 0)
+            {
+                foreach (var number in primeNumbers)
+                {
+                    Console.Write(number);
+                    if (number != primeNumbers.Last())
+                    {
+                        Console.Write(", ");
+                    }
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        public static List<uint> CreatePrimeNumbersSequence(uint n)
+        {
+            var primeNumbers = new List<uint>();
+
             var count = 0u;
             var i = 0u;
             while (count < n)
             {
                 if (IsPrimeNumber(i))
                 {
-                    Console.Write(i);
-                    if (count < n - 1)
-                    {
-                        Console.Write(", ");
-                    }
+                    primeNumbers.Add(i);
                     count++;
                 }
                 i++;
             }
 
-            Console.ReadLine();
+            return primeNumbers;
         }
 
         public static bool IsPrimeNumber(uint n)
