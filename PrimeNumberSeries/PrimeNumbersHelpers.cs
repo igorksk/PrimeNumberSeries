0efcfc376a8c923ba362780e260dc28c841ccb3f
Igor Kaseko <igorksk@gmail.com>
2024-08-11 10:26:33 +0300

    Update and refactor

---
+++ PrimeNumberSeries/PrimeNumbersHelpers.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Collections.Generic;
+
+namespace PrimeNumberSeries
+{
+    public class PrimeNumbersHelpers
+    {
+        public static List<uint> CreatePrimeNumbersSequence(uint n)
+        {
+            var primeNumbers = new List<uint>();
+
+            var count = 0u;
+            var i = 0u;
+            while (count < n)
+            {
+                if (IsPrimeNumber(i))
+                {
+                    primeNumbers.Add(i);
+                    count++;
+                }
+                i++;
+            }
+
+            return primeNumbers;
+        }
+
+        public static bool IsPrimeNumber(uint n)
+        {
+            var result = true;
+
+            if (n > 1)
+            {
+                for (var i = 2u; i < n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
--- PrimeNumberSeries/Program.cs
+++ PrimeNumberSeries/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace PrimeNumberSeries
@@ -12,7 +11,7 @@ namespace PrimeNumberSeries
             var n = Convert.ToUInt32(Console.ReadLine());
             Console.WriteLine("{0} first prime numbers", n);
             
-            var primeNumbers = CreatePrimeNumbersSequence(n);
+            var primeNumbers = PrimeNumbersHelpers.CreatePrimeNumbersSequence(n);
 
             if (primeNumbers.Count != 0)
             {
@@ -29,46 +28,6 @@ namespace PrimeNumberSeries
             Console.ReadLine();
         }
 
-        public static List<uint> CreatePrimeNumbersSequence(uint n)
-        {
-            var primeNumbers = new List<uint>();
-
-            var count = 0u;
-            var i = 0u;
-            while (count < n)
-            {
-                if (IsPrimeNumber(i))
-                {
-                    primeNumbers.Add(i);
-                    count++;
-                }
-                i++;
-            }
-
-            return primeNumbers;
-        }
-
-        public static bool IsPrimeNumber(uint n)
-        {
-            var result = true;
-
-            if (n > 1)
-            {
-                for (var i = 2u; i < n; i++)
-                {
-                    if (n % i == 0)
-                    {
-                        result = false;
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                result = false;
-            }
-
-            return result;
-        }
+       
     }
 }
